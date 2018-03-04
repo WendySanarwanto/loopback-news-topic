4d1ef195d6ec7de2b968b1020e2869eb94cf7048
WendySanarwanto <wendy.sanarwanto@gmail.com>
2018-03-04 11:46:26 +0800

    Added validation code to ensure that 'status' property is assigned with one of these values: 'publish', 'draft', 'deleted'

--- common/models/news.js
+++ common/models/news.js
@@ -1,5 +1,8 @@
 'use strict';
 
 module.exports = function(News) {
-
+  // Ensure that 'status' property is assigned with one of these values: 'publish', 'draft', 'deleted'
+  News.validatesInclusionOf('status', {
+    in: ['draft', 'publish', 'deleted']
+  });
 };
