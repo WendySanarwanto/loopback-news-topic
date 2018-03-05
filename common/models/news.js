4d96e0cda19cee69b4725c27648ee16240103963
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2018-03-05 20:03:20 +0800

    Added missing comma as suggested by eslint.

--- common/models/news.js
+++ common/models/news.js
@@ -3,6 +3,6 @@
 module.exports = function(News) {
   // Ensure that 'status' property is assigned with one of these values: 'publish', 'draft', 'deleted'
   News.validatesInclusionOf('status', {
-    in: ['draft', 'publish', 'deleted']
+    in: ['draft', 'publish', 'deleted'],
   });
 };
